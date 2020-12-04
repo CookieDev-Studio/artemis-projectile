a940ac2116f6c259fa72a10c7219d3306796c41d
Cameron <cameronproulx@outlook.com>
2020-12-04 13:10:19 -0500

    removed auto generated coments

--- Assets/Tests/CalculateTrajectoryTests.cs
+++ Assets/Tests/CalculateTrajectoryTests.cs
@@ -8,8 +8,6 @@ namespace Tests
 {
     public class CalculateTrajectoryTests
     {
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
         public IEnumerator FixedUpdateVelocityChange()
         {
